4765e49f336d7302ff17c1ce5fdb1383f1f05f9c
YaharaOctanis <yaharao55@gmail.com>
2017-01-03 21:30:51 +0100

    Finished Time class implementation.

--- Rusty SDL/RustyEngine/Time.cpp
+++ Rusty SDL/RustyEngine/Time.cpp
@@ -1,12 +1,31 @@
 #include "Time.h"
 
-using namespace RustyEngine;
+void RustyEngine::Time::init()
+{
+	RustyEngine::Time::start_t = SDL_GetPerformanceCounter();
+}
 
-Time::Time()
+void RustyEngine::Time::recalculate()
+{
+	using RustyEngine::Time;
+	curr_t = SDL_GetPerformanceCounter();
+	delta_t = (curr_t - last_t) / ((float)SDL_GetPerformanceFrequency());
+	last_t = curr_t;
+}
+
+void RustyEngine::Time::recalculate_fixed()
+{
+	using RustyEngine::Time;
+	fcurr_t = SDL_GetPerformanceCounter();
+	fixed_delta_t = (fcurr_t - flast_t) / ((float)SDL_GetPerformanceFrequency());
+	flast_t = fcurr_t;
+}
+
+RustyEngine::Time::Time()
 {
 }
 
 
-Time::~Time()
+RustyEngine::Time::~Time()
 {
 }
--- Rusty SDL/RustyEngine/Time.h
+++ Rusty SDL/RustyEngine/Time.h
@@ -3,13 +3,23 @@
 
 #pragma once
 
+#include <SDL.h>
+
 namespace RustyEngine
 {
-	// Holds information about time passed between frames
+	// Holds and calculates information about time passed between frames
 	// Use it for calculating movement of game objects
 	// Can be used to manipulate time in-game
 	class Time
 	{
+	private:
+		static float start_t;			// Timer tick at init
+		static float last_t;			// Timer tick at last timer update
+		static float curr_t;			// Current tick - used as temp var, for delta calculation
+
+		static float flast_t;			// Timer tick at last fixed timer update
+		static float fcurr_t;			// Current tick - used as temp var for fixed_delta calculation
+
 	public:
 		static float delta_t;			// Time between rendered frames
 		static float timescale;			// Multiplies delta_t (when calculating it)
@@ -17,6 +27,11 @@ namespace RustyEngine
 		static float fixed_delta_t;		// Time between fixed updates (physics)
 		static float fixed_timescale;	// Multiplies fixed_delta_t (when calculating it)
 
+		static void init();					// Set start_t (use after SDL init, and before level load)
+		static void recalculate();			// Calculate new delta_t (use at the end of render loop)
+		static void recalculate_fixed();	// Calculate new fixed_delta_t (use at the end of physics loop)
+
+	private:
 		Time();
 		~Time();
 	};
