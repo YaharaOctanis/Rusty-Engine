8321daf357bcb2bca7221e9b10e6baa51825e3f8
YaharaOctanis <yaharao55@gmail.com>
2017-01-06 11:38:21 +0100

    New input class, handles user inputs and special events (currently only mouse, touch and quit).

--- Rusty SDL/RustyEngine/Input.cpp
+++ Rusty SDL/RustyEngine/Input.cpp
@@ -1,7 +1,96 @@
 #include "Input.h"
+#include <SDL.h>
 
 namespace RustyEngine
 {
+	float Input::getMouseDown(Mousebutton button)
+	{
+		switch (button)
+		{
+			case left: return mouse_left;
+			case middle: return mouse_middle;
+			case right: return mouse_right;
+		}
+		return 0.0f;
+	}
+
+	const Vec2 & Input::getMousePos()
+	{
+		return mouse_pos;
+	}
+
+	float Input::getTouch()
+	{
+		return touch;
+	}
+
+	const Vec2 & Input::getTouchPos()
+	{
+		return touch_pos;
+	}
+
+	void Input::update()
+	{
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event))	// Grab input events
+		{
+			switch (event.type)
+			{
+				// Handle mouse button press
+				case SDL_MOUSEBUTTONDOWN:
+					mouse_pos.set(event.motion.x, event.motion.y);
+					
+					if (event.button.button & SDL_BUTTON_LMASK)
+						mouse_left = 1.0f;
+					if (event.button.button & SDL_BUTTON_MMASK)
+						mouse_middle = 1.0f;
+					if (event.button.button & SDL_BUTTON_RMASK)
+						mouse_right = 1.0f;
+					break;
+
+				// Handle mouse button release
+				case SDL_MOUSEBUTTONUP:
+					mouse_pos.set(event.motion.x, event.motion.y);
+
+					if (event.button.button & SDL_BUTTON_LMASK)
+						mouse_left = 0.0f;
+					if (event.button.button & SDL_BUTTON_MMASK)
+						mouse_middle = 0.0f;
+					if (event.button.button & SDL_BUTTON_RMASK)
+						mouse_right = 0.0f;
+					break;
+
+				// Handle mouse movement
+				case SDL_MOUSEMOTION:
+					mouse_pos.set(event.motion.x, event.motion.y);
+					break;
+
+				// Handle finger press
+				case SDL_FINGERDOWN:
+					touch_pos.set(event.tfinger.x, event.tfinger.y);
+					touch = 1.0f;
+					break;
+
+				// Handle finger release
+				case SDL_FINGERUP:
+					touch_pos.set(event.tfinger.x, event.tfinger.y);
+					touch = 0.0f;
+					break;
+
+				// Handle finger movement
+				case SDL_FINGERMOTION:
+					touch_pos.set(event.tfinger.x, event.tfinger.y);
+					break;
+
+
+				// Special system events
+				case SDL_QUIT:
+					exit(0);
+			}
+		}
+	}
+
 	Input::Input()
 	{
 	}
--- Rusty SDL/RustyEngine/Input.h
+++ Rusty SDL/RustyEngine/Input.h
@@ -7,12 +7,34 @@
 
 namespace RustyEngine
 {
+	enum Mousebutton : int
+	{
+		left = 0,
+		middle = 1,
+		right = 2
+	};
+
+	// Make event class and make sure this class extends it
 	class Input
 	{
 	private:
-		Vec2 mouse_pos;
+		static Vec2 mouse_pos;
+		static Vec2 touch_pos;
+		static float mouse_left;
+		static float mouse_right;
+		static float mouse_middle;
+		static float touch;
 
 	public:
+		static float getMouseDown(Mousebutton button);
+		static const Vec2& getMousePos();
+		static float getTouch();
+		static const Vec2& getTouchPos();
+
+		// TODO convert update private method and access it via friend class
+		static void update();	// Handles input events (convert to only handle input events, instead of all)
+
+	private:
 		Input();
 		~Input();
 	};
