c01aff79cf4865316f7217b6fc2472255ef85fd5
YaharaOctanis <yaharao55@gmail.com>
2017-01-21 14:36:27 +0100

    Added some comments.

--- Rusty SDL/RustyEngine/Audio.cpp
+++ Rusty SDL/RustyEngine/Audio.cpp
@@ -19,10 +19,10 @@ namespace RustyEngine
 	// Initialize audio subsystem
 	bool Audio::init()
 	{
-		frequency = 44100;
-		if (initialized)
+		if (initialized) // Do not initialize twice
 			return true;
 
+		// Open audio device channels
 		if (Mix_OpenAudio(frequency, format, channels, chunk_size) < 0)
 		{
 			std::cout << "AUDIO | Cannot initialize audio! SDL_mixer error: " << Mix_GetError() << std::endl;
@@ -36,9 +36,9 @@ namespace RustyEngine
 	// Does not handle clean-up of loaded audio chunks, you should do that before calling quit
 	bool Audio::quit()
 	{
-		Mix_HaltMusic();
+		Mix_HaltMusic();		// stop music
 		Mix_HaltChannel(-1);	// stop all channels
-		Mix_Quit();
+		Mix_Quit();				// stop mixer subsystem
 		return false;
 	}
 // -----------------------------------------------------------------------------------
@@ -62,7 +62,7 @@ namespace RustyEngine
 	// Destructor - cleans up audio data from memory
 	Audio::~Audio()
 	{
-		Mix_FreeChunk(sound);
+		Mix_FreeChunk(sound); // Remove audio data from memory
 		sound = nullptr;
 		loaded = false;
 	}
@@ -71,12 +71,13 @@ namespace RustyEngine
 	// Load sound effect from file
 	bool Audio::load(std::string file_path)
 	{
-		if (sound != nullptr)
+		if (sound != nullptr) // Is something already loaded?
 			return false; // unload here
 
 		filename = file_path;
-		sound = Mix_LoadWAV(filename.c_str());
+		sound = Mix_LoadWAV(filename.c_str()); // Load sound effect
 
+		// If load failed
 		if (sound == NULL)
 		{
 			std::cout << "AUDIO | Failed to load: " << filename << " " << Mix_GetError() << std::endl;
@@ -119,7 +120,7 @@ namespace RustyEngine
 	// Set playback volume (0 - 100)
 	void Audio::setVolume(int percent)
 	{
-		float vol = ((float)MIX_MAX_VOLUME) / 100.0f;
-		Mix_VolumeChunk(sound, roundf(vol * percent));
+		float vol = ((float)MIX_MAX_VOLUME) / 100.0f;  // Convert percent to mixer known value (MIX_MAX_VOLUME is not 100)
+		Mix_VolumeChunk(sound, roundf(vol * percent)); // Apply volume change
 	}
 }
--- Rusty SDL/RustyEngine/Time.cpp
+++ Rusty SDL/RustyEngine/Time.cpp
@@ -1,20 +1,22 @@
 #include "Time.h"
 
 // Private static member definitions
-Uint64 RustyEngine::Time::start_t;
-Uint64 RustyEngine::Time::last_t;
-Uint64 RustyEngine::Time::curr_t;
+Uint64 RustyEngine::Time::start_t;	// Timer tick at init
+Uint64 RustyEngine::Time::last_t;	// Timer tick at last timer update
+Uint64 RustyEngine::Time::curr_t;	// Current tick - used as temp var, for delta calculation
 
-Uint64 RustyEngine::Time::flast_t;
-Uint64 RustyEngine::Time::fcurr_t;
+Uint64 RustyEngine::Time::flast_t;	// Timer tick at last fixed timer update
+Uint64 RustyEngine::Time::fcurr_t;	// Current tick - used as temp var for fixed_delta calculation
 
 
 // Public static member definitions
-float RustyEngine::Time::delta_t;
-float RustyEngine::Time::timescale;
+float RustyEngine::Time::delta_t;	// Time between rendered frames
+float RustyEngine::Time::timescale;	// Multiplies delta_t (when calculating it) - avoid high values, since precision loss can occur 
+									// Negative values are allowed, but not recommended (they will not reverse time, but only result in negative delta)
 
-float RustyEngine::Time::fixed_delta_t;
-float RustyEngine::Time::fixed_timescale;
+float RustyEngine::Time::fixed_delta_t;		// Time between fixed updates (physics)
+float RustyEngine::Time::fixed_timescale;	// Multiplies fixed_delta_t (when calculating it) - avoid high values, since precision loss can occur
+											// Negative values are allowed, but not recommended (they will not reverse time, but only result in negative delta)
 
 
 
@@ -23,15 +25,15 @@ float RustyEngine::Time::fixed_timescale;
 // Set static values (use after SDL init, and before level load)
 void RustyEngine::Time::init()
 {
-	RustyEngine::Time::start_t = SDL_GetPerformanceCounter();
-	RustyEngine::Time::curr_t = RustyEngine::Time::start_t;
+	RustyEngine::Time::start_t = SDL_GetPerformanceCounter();	// Remember starting tick
+	RustyEngine::Time::curr_t = RustyEngine::Time::start_t;		// Set everything to starting tick
 	RustyEngine::Time::last_t = RustyEngine::Time::start_t;
 	RustyEngine::Time::fcurr_t = RustyEngine::Time::start_t;
 	RustyEngine::Time::flast_t = RustyEngine::Time::start_t;
-	RustyEngine::Time::delta_t = 0;
-	RustyEngine::Time::fixed_delta_t = 0;
-	RustyEngine::Time::timescale = 1;
-	RustyEngine::Time::fixed_timescale = 1;
+	RustyEngine::Time::delta_t = 0;			// Delta is 0 when starting of course
+	RustyEngine::Time::fixed_delta_t = 0;	// Same goes for fixed delta
+	RustyEngine::Time::timescale = 1;		// Default timescale is 1
+	RustyEngine::Time::fixed_timescale = 1;	// Default fixed timescale is also 1
 }
 
 
@@ -55,10 +57,11 @@ void RustyEngine::Time::recalculateFixed()
 }
 
 
+// TODO - FIX
 // Returns time in seconds since game launched (since init() was called)
 double RustyEngine::Time::timeSinceStartup()
 {
-	// Calculate time with doubles, then return float
+	// Calculate time with doubles, then return float ( (start - curr) / freq )
 	return (double)(RustyEngine::Time::start_t - SDL_GetPerformanceCounter()) / (double)SDL_GetPerformanceFrequency();
 }
 
